51f3e1f9cacd58307a31de36db5ad7636ae578e6
frandreoli <fra.andreoli@hotmail.it>
2023-09-15 22:07:40 +0200

    first commit

---
+++ Functions.jl
@@ -0,0 +1,314 @@
+#
+############################### Generic functions definitions ########################################
+#
+#
+function euclidean_distance(values)
+    return sqrt(sum(values.^2))
+end
+#
+function uniform_box(n_value, x_length,y_length,z_length)
+    r_atoms = Array{Float64}(undef,n_value,3)
+    r_atoms[:,1] = (rand(n_value).-0.5).*x_length
+    r_atoms[:,2] = (rand(n_value).-0.5).*y_length
+    r_atoms[:,3] = (rand(n_value).-0.5).*z_length
+    return r_atoms
+end
+#
+function array_replace!(array,a,b)
+    value_temp=array[a,:]
+    array[a,:]=array[b,:]
+    array[b,:]=value_temp
+end
+#
+#
+#
+############################### Joint algorithm definitions ########################################
+#
+#
+function sample_sphere_joint(n_start, distance_threshold, distance_function , sampling_function, sampling_options, time_threshold=60, print_option=false, check_option=false)
+    #
+    function println_if(string...)
+        print_option ? println(string...) : nothing
+    end
+    #
+    println_if("Starting evaluation core.")
+    #
+    n_value = n_start
+    sampled_array = sampling_function(n_start, sampling_options...)
+    #
+    time_start=time()
+    #
+    #
+    remain  = n_value
+    i_old   = 0
+    i_subs = n_value
+    time_condition = true
+    #
+    while remain>0 && time_condition
+        i_subs = n_value 
+        #
+        for i in 1:i_subs
+            #
+            start_point = max(i,i_old)
+            #
+            for j in (start_point + 1):i_subs
+                while distance_function(sampled_array[i,:]-sampled_array[j,:])<distance_threshold && j<=i_subs            
+                    array_replace!(sampled_array,j,i_subs)
+                    i_subs-=1
+                end
+            end
+        end
+        #
+        remaining_i = (i_subs+1):n_value
+        sampled_array[remaining_i,:] = sampling_function(length(remaining_i), sampling_options...)
+        remain = length(remaining_i)
+        #
+        println_if("Values remaining to sample: ",remain,".\nPoints already sampled: ",i_subs,".\nComputation_valuel time: ",time()-time_start)
+        #
+        i_old = i_subs
+        #
+        time_condition = (time()-time_start)<=time_threshold
+        #
+        if !time_condition
+            @warn "Exceeded computation_value time (joint). Number of points correctly sampled: "*string(i_subs)
+            n_value=i_subs
+            sampled_array = sampled_array[1:n_value,:]
+        end
+        #
+    end
+    #
+    core_computation_time=(time()-time_start)
+    #
+    if check_option
+        remain=0
+        println_if("Starting consistency check.")
+        for i = 1:n_value
+            for j = i + 1:n_value #i<j<=n_value
+                if euclidean_distance(sampled_array[i,:]-sampled_array[j,:])<distance_threshold
+                    remain=remain+1
+                end
+            end
+        end
+        if remain!=0
+            @warn "Code failure. Number of exceptions: "*string(remain)
+        end
+    end
+    #
+    println_if("Number of exceptions: ",remain)
+    println_if("Computation correctly completed.\nCore evaluation computation_valuel time: ", core_computation_time,"\nOverall computation_valuel time: ", time()-time_start)
+    #
+    return sampled_array, n_value 
+end
+#
+#
+#
+############################### Single algorithm definitions ########################################
+#
+#
+function sample_sphere_single(n_start, distance_threshold, distance_function , sampling_function, sampling_options, time_threshold=60, print_option=false, check_option=false)
+    #
+    function println_if(string...)
+        print_option ? println(string...) : nothing
+    end
+    #    
+    sampled_array = sampling_function(1, sampling_options...)
+    time_condition = true
+    time_start = time()
+    n_value = 1
+    #
+    while n_value<n_start && time_condition
+        keep_sampling = true
+        while keep_sampling
+            keep_sampling = false
+            sampled_point = sampling_function(1, sampling_options...)
+            for i in 1:n_value
+                if distance_function(sampled_array[i,:]-sampled_point[1,:]) < distance_threshold
+                    keep_sampling = true
+                    break
+                end
+            end
+            if !keep_sampling
+                sampled_array = vcat(sampled_array,sampled_point)
+            end
+        end
+        n_value = length(sampled_array[:,1])
+        #
+        time_condition = (time()-time_start)<=time_threshold
+        if !time_condition
+            @warn "Exceeded computation_value time (single). Number of atoms correctly sampled: "*string(n_start-n_value)
+            sampled_array = sampled_array[1:n_value,:]
+        end
+    end
+    #
+    if check_option
+        remain=0
+        println_if("Starting consistency check.")
+        for i = 1:n_value
+            for j = i + 1:n_value 
+                if euclidean_distance(sampled_array[i,:]-sampled_array[j,:])<distance_threshold
+                    remain=remain+1
+                end
+            end
+        end
+        if remain!=0
+            @warn "Code failure. Number of exceptions: "*string(remain)
+        end
+    end
+    #
+    return sampled_array, n_value 
+end
+#
+#
+#
+############################### Smart algorithm definitions ########################################
+#
+#
+function point_sample(n_value, distance, indices)
+    n_dims = length(indices)
+    return indices
+end
+#
+function region_shape_cube(vec, sizes)
+    dims = length(sizes)
+    valid = true
+    for i in 1:dims
+        if vec[i]<0||vec[i]>sizes[i]
+            valid = false
+            break
+        end
+    end
+    return valid
+end
+#
+function valid_neighbour(i_neigh,dim_points )
+    valid = true
+    for i in 1:length(i_neigh)
+        if i_neigh[i]<1 || i_neigh[i]>dim_points[i]
+            valid = false
+            break
+        end
+    end
+    return valid
+end
+#
+function sample_sphere_smart(n_start, distance_threshold, distance_function, region_shape_func, enclosing_cube::Tuple, time_threshold=60, print_option=false, check_option=false)
+    #
+    #enclosing_cube = collect(enclosing_cube_start)
+    #
+    dimensions = length(enclosing_cube)
+    neighbour_list = collect(Iterators.product([(0,1,-1) for i in 1:dimensions]... ))[2:end]
+    #
+    function println_if(string...)
+        print_option ? println(string...) : nothing
+    end
+    #
+    #
+    dim_points = (x->ceil(Int, x/distance_threshold)).(enclosing_cube)
+    #
+    grid_points  = Array{Float64}(undef, dim_points..., 3)
+    grid_points .= 0.0
+    #
+    grid_indices_values = collect(Iterators.product((x->collect(1:x)).(dim_points)... ))
+    grid_indices_availability  = Array{Bool}(undef, dim_points...)
+    grid_indices_availability .= true
+    #
+    #all_indices_list = (x->collect(1:x)).(dim_points)
+    #available_indices = (x->[true for i in 1:x]).(dim_points)
+    #
+    #print(available_indices)
+    #filled_indices = []
+    #
+    #i_list = sampled_point = Tuple(zeros(length(available_indices)))
+    #
+    #point_rejection = true
+    #time_condition = true
+    #time_start = time()
+    #while point_rejection && time_condition
+    #    i_list = rand.(all_indices_list)
+    #    sampled_point = ((i_list .- rand(dimensions)).*distance_threshold)
+    #    point_rejection = !region_shape_func(sampled_point)
+    #    time_condition = (time()-time_start)<=time_threshold
+   # end
+    #
+    #if !point_rejection
+    #    grid_points[i_list...,:] = sampled_point[:]
+    #    for dims in dimensions
+    #        (available_indices[dims])[i_list[dims]] = false
+    #        filled_indices[dims] .= push!(filled_indices[dims], i_list[dims])
+    #    end
+    #else
+    #    error("Problem in sampling the first point: run out of time.")
+    #end
+    #
+    n_value = 0
+    #
+    #
+    time_condition = true
+    time_start = time()
+    while n_value<n_start && time_condition
+        point_rejection = true
+        while point_rejection && time_condition
+            #
+            i_list = rand((grid_indices_values[grid_indices_availability])[:])
+            sampled_point = (i_list .- rand(dimensions)).*distance_threshold
+            point_rejection = !region_shape_func(sampled_point)
+            #
+            if !point_rejection  
+                #
+                i_near_list = (x->i_list.+x).(neighbour_list)
+                i_near_list = i_near_list[(x->valid_neighbour(x,dim_points)).(i_near_list)]
+                i_near_list = i_near_list[(x->grid_points[x...,:]!=[0.0;0.0;0.0]).(i_near_list)]
+                #
+                for i_near in i_near_list
+                    if distance_function(grid_points[i_near...,:]-sampled_point)<distance_threshold
+                        point_rejection = true
+                        break
+                    end
+                end
+            end
+            #
+            if !point_rejection
+                grid_points[i_list...,:] = sampled_point[:]
+                grid_indices_availability[i_list...] = false
+                n_value+=1
+            end
+            time_condition = (time()-time_start)<=time_threshold
+        end
+    end
+    #
+    #
+    correct_indices = (grid_indices_values[.!grid_indices_availability])[:]
+    #
+    if !time_condition
+        @warn "Exceeded computation_value time (joint). Number of points correctly sampled: "*string(n_value)
+    else
+        length(correct_indices[:]) != n_start || n_value!=n_start ? error("Wrong amount of sampled spheres.") : nothing
+    end
+    #    
+    sampled_array = Array{Float64}(undef, n_start, 3)
+    for i in 1:n_start
+        sampled_array[i,:] = grid_points[correct_indices[i]...,:]
+    end
+    #
+    #
+    if check_option
+        remain=0
+        println_if("Starting consistency check.")
+        for i = 1:n_start
+            for j = (i + 1):n_start 
+                if euclidean_distance(sampled_array[i,:]-sampled_array[j,:])<distance_threshold
+                    remain+=1
+                    println_if("Exception at: ", sampled_array[i,:], " and ", sampled_array[j,:])
+                end
+            end
+        end
+        if remain!=0
+            @warn "Code failure. Number of exceptions: "*string(remain)
+        end
+        println_if("End consistency check.")
+    end
+    #
+    return sampled_array, n_value 
+end
+#
+#
---
+++ Launcher.jl
@@ -0,0 +1,180 @@
+#
+using Plots
+using CurveFit, Statistics
+include("Functions.jl")
+#
+#
+############################### Test parameters ########################################
+#
+#
+verbose_option = [true ; false][1]
+#
+x_length = 1.
+y_length = 1.
+z_length = 1.
+#
+filling = 0.8#0.95
+n_array = [10, 20,  40 , 60 ,80, 100]#; 300]#; 1000]
+#
+n_repetitions = 1000
+max_time = 300#180
+#
+#
+############################### Initializing elements ########################################
+#
+#
+n_length = length(n_array)
+cube_values = (x_length,y_length,z_length)
+distance_func = (xx-> ((x_length*y_length*z_length/xx)^(1/3))*filling)
+distance_array = distance_func.(n_array)
+#
+#
+exit_array_joint = [true for i in 1:n_length]
+exit_array_single = [true for i in 1:n_length]
+exit_array_smart = [true for i in 1:n_length]
+#
+time_array_joint = zeros(n_length)
+time_array_single = zeros(n_length)
+time_array_smart = zeros(n_length)
+#
+time_array_joint_std = zeros(n_length)
+time_array_single_std = zeros(n_length)
+time_array_smart_std = zeros(n_length)
+#
+time_array_joint_std_log = zeros(n_length)
+time_array_single_std_log = zeros(n_length)
+time_array_smart_std_log = zeros(n_length)
+#
+time_array_joint_list = zeros(n_length,n_repetitions)
+time_array_single_list = zeros(n_length,n_repetitions)
+time_array_smart_list = zeros(n_length,n_repetitions)
+#
+sample_sphere_joint(n_array[1], distance_array[1], euclidean_distance , uniform_box, cube_values, max_time, false, true)
+sample_sphere_single(n_array[1], distance_array[1], euclidean_distance , uniform_box, cube_values, max_time, false, true)
+sample_sphere_smart(n_array[1], distance_array[1], euclidean_distance, x->(region_shape_cube(x,cube_values)), cube_values, max_time, false, true)
+#
+#
+############################### Testing the algorithms ########################################
+#
+#
+if verbose_option
+    println("The number of repetitions is ", n_repetitions)
+end
+#
+#
+for i_main in 1:length(n_array)
+    #
+    n_main = n_array[i_main]
+    distance_threshold = distance_array[i_main]
+    if verbose_option
+        println("Starting n_main = ", n_main, ". Distance threshold: ", distance_threshold)
+    end
+    for i_rep in 1:n_repetitions
+        if verbose_option
+            println("-- Starting repetition ", i_rep)
+            flush(stdout)
+        end
+        #
+        #
+        time_start_joint = time()
+        sampled_array_joint_val, n_value_joint_val = sample_sphere_joint(n_main, distance_threshold, euclidean_distance , uniform_box, (x_length,y_length,z_length), max_time)
+        time_array_joint_list[i_main,i_rep] = time()-time_start_joint
+        #
+        time_start_single = time()
+        sampled_array_single_val, n_value_single_val = sample_sphere_single(n_main, distance_threshold, euclidean_distance , uniform_box, (x_length,y_length,z_length), max_time)
+        time_array_single_list[i_main,i_rep] = time()-time_start_single
+        #
+        time_start_smart = time()
+        sampled_array_smart_val, n_value_smart_val = sample_sphere_smart(n_main, distance_threshold, euclidean_distance, x->(region_shape_cube(x,cube_values)), cube_values, max_time)
+        time_array_smart_list[i_main,i_rep] = time()-time_start_smart
+        #
+        #Testing the joint code
+        errors_joint = 0
+        for i in 1:n_value_joint_val, j in (i + 1):n_value_joint_val
+            if euclidean_distance(sampled_array_joint_val[i, : ]-sampled_array_joint_val[j,:]) < distance_threshold
+                errors_joint+=1
+            end
+        end
+        if errors_joint>0
+            error("-- Problem with n_val = ", n_main," (joint). Found ", errors_joint," exceptions.")
+            flush(stdout)
+            exit_array_joint[i_main]= false
+        end
+        #
+        #Testing the single code
+        errors_single = 0
+        for i in 1:n_value_single_val, j in (i + 1):n_value_single_val 
+            if euclidean_distance(sampled_array_single_val[i, : ]-sampled_array_single_val[j,:]) < distance_threshold
+                errors_single+=1
+            end
+        end
+        if errors_single>0
+            error("-- Problem with n_val = ", n_main," (single). Found ", errors_single," exceptions.")
+            flush(stdout)
+            exit_array_single[i_main]= false
+        end
+        #
+        #Testing the smart code
+        errors_smart = 0
+        for i in 1:n_value_smart_val, j in (i + 1):n_value_smart_val 
+            if euclidean_distance(sampled_array_smart_val[i, : ]-sampled_array_smart_val[j,:]) < distance_threshold
+                errors_smart+=1
+            end
+        end
+        if errors_smart>0
+            error("-- Problem with n_val = ", n_main," (smart). Found ", errors_smart," exceptions.")
+            flush(stdout)
+            exit_array_smart[i_main]= false
+        end        
+        #
+    end
+    #
+    time_array_joint[i_main] = mean(time_array_joint_list[i_main,:])
+    time_array_single[i_main]= mean(time_array_single_list[i_main,:])
+    time_array_smart[i_main]= mean(time_array_smart_list[i_main,:])
+    time_array_joint_std[i_main] = std(time_array_joint_list[i_main,:])
+    time_array_single_std[i_main]= std(time_array_single_list[i_main,:])
+    time_array_smart_std[i_main]= std(time_array_smart_list[i_main,:])
+    time_array_joint_std_log[i_main] = std(log.(time_array_joint_list[i_main,:]))
+    time_array_single_std_log[i_main]= std(log.(time_array_single_list[i_main,:]))
+    time_array_smart_std_log[i_main]= std(log.(time_array_smart_list[i_main,:]))
+    #
+    if verbose_option 
+        println("Finished.\n")
+    end
+    #
+    #
+end
+#
+#
+#
+#Printing values
+println("N values: ", n_array, ", repetitions: ",n_repetitions,", filling: ", filling)
+println("joint times: ", time_array_joint)
+println("         +/- ", time_array_joint_std)
+println("   (log) +/- ", time_array_joint_std_log)
+println("single times: ", time_array_single)
+println("         +/- ", time_array_single_std)
+println("   (log) +/- ", time_array_single_std_log)
+println("smart times: ", time_array_smart)
+println("         +/- ", time_array_smart_std)
+println("   (log) +/- ", time_array_smart_std_log)
+#
+#
+#Fitting the scaling
+start_i_fit = 2
+fit_joint = linear_fit(log.(n_array[start_i_fit:end]), log.(time_array_joint[start_i_fit:end]))
+fit_single = linear_fit(log.(n_array[start_i_fit:end]), log.(time_array_single[start_i_fit:end]))
+fit_smart = linear_fit(log.(n_array[start_i_fit:end]), log.(time_array_smart[start_i_fit:end]))
+println("fit_joint = ", fit_joint)
+println("fit_single = ", fit_single)
+println("fit_smart = ", fit_smart)
+#
+#
+#Final plot
+start_i_plot = 2
+plot(log.(n_array[start_i_plot:end]), log.(time_array_joint[start_i_plot:end]), label="joint", seriestype=:scatter, yerror=time_array_joint_std_log[start_i_plot:end])
+plot!(log.(n_array[start_i_plot:end]), log.(time_array_single[start_i_plot:end]), label="single", seriestype=:scatter, yerror=time_array_single_std_log[start_i_plot:end])
+plot!(log.(n_array[start_i_plot:end]), log.(time_array_smart[start_i_plot:end]), label="smart", seriestype=:scatter, yerror=time_array_smart_std_log[start_i_plot:end])
+ylabel!("log(T)")
+xlabel!("log(N)")
